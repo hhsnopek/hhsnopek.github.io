69e910fed80005a29410406fc013623764ae9f9d
HHSnopek <hhsnopek@gmail.com>
2013-08-10 20:57:38 -0500

    modified:   assets/css/menu.styl Brand new design 	modified:   assets/favicon.ico New Favicon

--- assets/js/main.coffee
+++ assets/js/main.coffee
@@ -1,5 +1,42 @@
+require.config(
+	paths:
+		jquery: '../components/jquery/jquery.min'
+		underscore: '../components/underscore/underscore'
+		Backbone: '../components/backbone/backbone'
+		JSONP: '../js/jsonp'
 
-js = ["http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"]
+	shim:
+		underscore:
+			exports: '_'
+		Backbone:
+			deps: ['underscore', 'jquery']
+			exports: 'Backbone'
+)
 
-require js, ->
-  return
+require [
+	'jquery',
+	'Backbone',
+	'JSONP',
+], ($, Backbone, JSONP) ->
+	console.log "Loaded: main.coffee"
+
+	jsonp.get("#{BACKEND_URL}/?json=1", {}, (data) ->
+		console.log data
+		for post in data['posts']
+			$('body').append(
+				"""
+				<section>
+					<h1 class="title">#{post['title']}</h1>
+					<span class="author">by #{post['author']['name']}</span>
+					#{post['content']}
+				<section>
+				"""
+			)
+	)
+
+# Scroll effects
+	$(window).scroll ->
+		height = $(window).scrollTop()
+		#displayimages() if height > 1000
+		
+	console.log height
--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,40 @@
-!function(){var a;a=["http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"],require(a,function(){})}.call(this);
+(function() {
+  require.config({
+    paths: {
+      jquery: '../components/jquery/jquery.min',
+      underscore: '../components/underscore/underscore',
+      Backbone: '../components/backbone/backbone',
+      JSONP: '../js/jsonp'
+    },
+    shim: {
+      underscore: {
+        exports: '_'
+      },
+      Backbone: {
+        deps: ['underscore', 'jquery'],
+        exports: 'Backbone'
+      }
+    }
+  });
+
+  require(['jquery', 'Backbone', 'JSONP'], function($, Backbone, JSONP) {
+    console.log("Loaded: main.coffee");
+    jsonp.get("" + BACKEND_URL + "/?json=1", {}, function(data) {
+      var post, _i, _len, _ref, _results;
+      console.log(data);
+      _ref = data['posts'];
+      _results = [];
+      for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+        post = _ref[_i];
+        _results.push($('body').append("<section>\n	<h1 class=\"title\">" + post['title'] + "</h1>\n	<span class=\"author\">by " + post['author']['name'] + "</span>\n	" + post['content'] + "\n<section>"));
+      }
+      return _results;
+    });
+    $(window).scroll(function() {
+      var height;
+      return height = $(window).scrollTop();
+    });
+    return console.log(height);
+  });
+
+}).call(this);
