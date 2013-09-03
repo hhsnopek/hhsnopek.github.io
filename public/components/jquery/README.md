f7d9a5d569d6c6374a89bae28c48873e1189bca8
HHSnopek <hhsnopek@gmail.com>
2013-09-03 12:22:34 -0500

    Latest blog!

--- public/components/jquery/README.md
+++ public/components/jquery/README.md
@@ -1,11 +1,1 @@
-jQuery Component
-================
-
-Shim repository for the [jQuery](http://jquery.com).
-
-Package Managers
-----------------
-
-* [Bower](http://bower.io/): `jquery`
-* [Component](https://github.com/component/component): `components/jquery`
-* [Composer](http://packagist.org/packages/components/jquery): `components/jquery`
+/usrdata/web/hhsnopek.com/assets/components/jquery/README.md
--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,34 @@
-!function(){require.config({paths:{jquery:"//cdnjs.cloudflare.com/ajax/libs/jquery/2.0.3/jquery.min",moment:"//cdnjs.cloudflare.com/ajax/libs/moment.js/2.1.0/moment.min"}}),require(["jquery","moment"],function(e,t){return e.ajax({url:"/data/blog.json",cache:!1}).done(function(n){var o,l,c,s,i,r;for(i=n.posts,r=[],c=0,s=i.length;s>c;c++)l=i[c],o=t(l.datetime).fromNow(),r.push(e("#blog").append('<section class="post">\n  <h1 class="title">'+l.title+'</h1>\n  <h5 class="datetime">'+o+"</h5>\n  "+l.content+"\n<section>"));return r}),e("#blog-link").click(function(){return document.getElementById("projects").style.display="none",document.getElementById("blog").style.display="block"}),e("#projects-link").click(function(){return document.getElementById("blog").style.display="none",document.getElementById("projects").style.display="block"})})}.call(this);
+(function() {
+  require.config({
+    paths: {
+      jquery: '//cdnjs.cloudflare.com/ajax/libs/jquery/2.0.3/jquery.min',
+      moment: '//cdnjs.cloudflare.com/ajax/libs/moment.js/2.1.0/moment.min'
+    }
+  });
+
+  require(['jquery', 'moment'], function($, moment) {
+    $.ajax({
+      url: '/data/blog.json',
+      cache: false
+    }).done(function(data) {
+      var date, post, _i, _len, _ref, _results;
+      _ref = data['posts'];
+      _results = [];
+      for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+        post = _ref[_i];
+        date = moment(post['datetime']).fromNow();
+        _results.push($('#blog').append("<section class=\"post\">\n  <h1 class=\"title\">" + post['title'] + "</h1>\n  <h5 class=\"datetime\">" + date + "</h5>\n  " + post['content'] + "\n<section>"));
+      }
+      return _results;
+    });
+    $('#blog-link').click(function() {
+      document.getElementById('projects').style.display = 'none';
+      return document.getElementById('blog').style.display = 'block';
+    });
+    return $('#projects-link').click(function() {
+      document.getElementById('blog').style.display = 'none';
+      return document.getElementById('projects').style.display = 'block';
+    });
+  });
+
+}).call(this);
